cd676d633ec2b66926bf5862ef4b2cc5692c5fc0
alreva <alreva@gmail.com>
2022-04-04 21:21:23 +0000

    Deploying to gh-pages from  @ 2555f7708182c32e0180884bf860baa38fe0ee0c 🚀

---
+++ copy-to-clipboard.js
@@ -0,0 +1,11 @@
+﻿window.clipboardCopy = {
+    copyText: function(text) {
+        navigator.clipboard.writeText(text)
+            .then(function () {
+                console.log("Copied text: " + text);
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};
